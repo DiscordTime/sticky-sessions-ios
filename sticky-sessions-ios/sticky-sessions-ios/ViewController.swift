ac255fc6f40f7e013f92dc17f9d88b1e2528c626
Vinicius Albuquerque <viniciusalbuquerque@Viniciuss-MacBook-Pro.local>
2018-10-22 21:52:33 -0300

    Initial configuration
    Starting an empty iOS project.

---
+++ sticky-sessions-ios/sticky-sessions-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sticky-sessions-ios
+//
+//  Created by Vinicius Albuquerque on 10/22/18.
+//  Copyright © 2018 DiscordTime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
