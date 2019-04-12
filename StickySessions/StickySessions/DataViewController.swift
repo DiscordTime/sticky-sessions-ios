5d52a042073613b898ef76df54c2b910ef152d9a
Patrick Steiger <psteiger@gmail.com>
2019-04-12 10:28:57 -0300

    Project skeleton created.

---
+++ StickySessions/StickySessions/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  StickySessions
+//
+//  Created by Patrick Steiger on 12/04/19.
+//  Copyright © 2019 CESAR. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
