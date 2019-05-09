8047785d55c0bf5459fc9e34f19c66be75b52eed
antoniosj <antonio.silvajr23@gmail.com>
2019-05-09 01:08:36 -0300

    WIP: Add note

--- StickySessions/StickySessions/Repository/Repository.swift
+++ StickySessions/StickySessions/Repository/Repository.swift
@@ -8,7 +8,7 @@
 
 protocol Repository {
     func fetch(urlStr: String, onResponse: OnResponse)
-    func add(urlStr: String, note: Note, onResponse: OnResponse)
+    func add(urlStr: String, params: [String], onResponse: OnResponse)
 }
 
 protocol OnResponse {
